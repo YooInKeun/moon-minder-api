aa79e4994619d4264596f308b66c07a681387b0a
유인근 <keun0390@naver.com>
2021-01-16 23:56:11 +0900

    feat: add moon-minder-api project 3699d0d

---
+++ src/main/java/com/moonminder/api/web/MoonMinderController.java
@@ -0,0 +1,15 @@
+package com.moonminder.api.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MoonMinderController {
+
+    @GetMapping
+    public String testConnection() {
+        return "Success!";
+    }
+}
