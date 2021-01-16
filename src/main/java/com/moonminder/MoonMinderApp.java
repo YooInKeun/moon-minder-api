3699d0d9fe5ba920a45b7d856463b6a7e0075c1e
유인근 <keun0390@miridih.com>
2021-01-16 18:34:07 +0900

    feat: add moon-minder-api project

---
+++ src/main/java/com/moonminder/MoonMinderApp.java
@@ -0,0 +1,11 @@
+package com.moonminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonMinderApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MoonMinderApp.class, args);
+    }
+}
