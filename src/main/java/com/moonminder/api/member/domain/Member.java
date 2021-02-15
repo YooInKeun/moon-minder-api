500999851640b756a073e0d74976034ce1b8cde6
YooInKeun <keun0390@naver.com>
2021-02-16 01:01:07 +0900

    feat: add field in member entity

--- src/main/java/com/moonminder/api/member/domain/Member.java
+++ src/main/java/com/moonminder/api/member/domain/Member.java
@@ -9,26 +9,35 @@ import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
-@Entity
+@Entity(name = "Member")
 public class Member extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private long id;
 
-    @Column(nullable = false)
+    @Column(name = "NAME", nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(name = "EMAIL", unique = true, nullable = false)
     private String email;
 
-    @Column
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+
+    @Column(name = "PICTURE")
     private String picture;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "TYPE", nullable = false)
+    private MemberType type;
+
     @Builder
-    public Member(String name, String email, String picture) {
+    public Member(String name, String email, String password, String picture, MemberType type) {
         this.name = name;
         this.email = email;
+        this.password = password;
         this.picture = picture;
+        this.type = type;
     }
 }
