87c869fad9ff2106f7bc88439c5ca30f47716944
YooInKeun <keun0390@naver.com>
2021-02-17 00:57:18 +0900

    feat: add logout implementation

--- src/main/java/com/moonminder/api/member/web/MemberApiController.java
+++ src/main/java/com/moonminder/api/member/web/MemberApiController.java
@@ -3,8 +3,14 @@ package com.moonminder.api.member.web;
 import com.moonminder.api.member.service.MemberService;
 import com.moonminder.api.member.web.dto.SignupRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,6 +21,12 @@ public class MemberApiController {
     @PostMapping("/signup")
     public String signup(SignupRequest signupRequest) {
         memberService.save(signupRequest.toMember());
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
